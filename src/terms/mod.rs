b9029ae96bc6616a4de60a88c1f4c595476ffc57
David Heise <heiseda@ornl.gov>
2015-03-17 14:12:56 -0400

    Added a term access example. The acessor is kind of kludgy.

--- src/terms/mod.rs
+++ src/terms/mod.rs
@@ -6,37 +6,46 @@
 
 //! Provide basic definitions of terms.
 
+use std::collections::BitVec;
+//use std::tuple::Tuple1;
+
 /// Represents root terms.
-struct ERoot ( pub () );
-struct EAny ( pub () );
-struct ENone ( pub () );
+struct Root ( () );
+struct Any ( () );
+struct ENone ( () );
 #[derive(Debug)]
-struct EString(pub String);
-struct Symbol(pub String);
+struct EString( String ) ;
+struct Symbol( String );
 #[derive(Debug)]
-struct Integer(pub isize);
-struct Float(pub f64);
-struct BitString(pub Vec<i8>);
-struct Boolean(pub bool);
+struct Integer( isize );
+struct Float( f64 );
+struct BitString( BitVec );
+struct Boolean( bool );
 
 #[derive(Debug)]
 struct Term<T> {
     value: T,
 }
 
-impl<T> Term<T> {
+impl<T> Term<T>
+//where T: Tuple1
+{
     pub fn new(t: T) -> Term<T> {
         Term { value: t }
     }
+    pub fn unwrap(&self) -> &T {
+        &self.value
+    }
 }
+
+
 
 #[test]
 fn term_type_check_test() -> (){
     let stringterm = Term::new(EString("test".to_string()));
     let nestedterm = Term::new(Term::new(Integer(3)));
-    
-    //let rootkind = rootterm.get_kind();
-    //println!("{:?}", rootkind);
-    panic!("Intential fail. {:?}", nestedterm);
+    //how to make this access not require .0?
+    let stringval = &stringterm.unwrap().0;
+    panic!("Intentionial panic. {:?}", stringval);
 }
 
